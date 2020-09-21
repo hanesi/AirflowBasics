3e7f3cb118c48d609e22c9418990acc8b1899b85
Ian Hanes <ian@sharelocalmedia.com>
2020-09-21 09:09:47 -0400

    add simple dag

---
+++ dags/SimpleDAG.py
@@ -0,0 +1,20 @@
+from airflow.models import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.python_operator import PythonOperator
+args = {
+    "owner": "ian",
+    "startDate": days_ago(1)
+}
+dag = DAG(dag_id="SimpleDAG", default=args, schedule_interval=None)
+
+
+def run_this_func(**context):
+    print("Hi")
+
+
+with dag:
+    run_this_task = PythonOperator(
+        task_id='run_this',
+        python_callable=run_this_func,
+        provide_context=True
+    )
